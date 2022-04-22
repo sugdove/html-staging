793e2a3d8d0862f49837cc70bcceca9e9580eeb5
suge <849809724@qq.com>
2022-04-22 15:43:40 +0800

    add some

--- server.js
+++ server.js
@@ -2,43 +2,38 @@ const express = require('express')
 
 const app = express()
 
-app.get('/test',(request, response)=>{
-
-  response.setHeader('Access-Control-Allow-Origin','*')
+app.get('/test', (request, response) => {
+  response.setHeader('Access-Control-Allow-Origin', '*')
 
   response.send('Hello express get')
 })
 
-app.post('/post',(request, response)=>{
+app.post('/post', (request, response) => {
+  response.setHeader('Access-Control-Allow-Origin', '*')
 
-  response.setHeader('Access-Control-Allow-Origin','*')
-  
   const obj = {
-    message: 'Hello express post'
+    message: 'Hello express post',
   }
 
-  setTimeout(()=>{
-
+  setTimeout(() => {
     response.send(JSON.stringify(obj))
-
-  },3000)
+  }, 3000)
 
   // response.send('Hello express post')
 })
 
-app.all('/jsonp',(request, response)=>{
-
+app.all('/jsonp', (request, response) => {
   // response.setHeader('Access-Control-Allow-Origin','*')
-  
+
   const obj = {
-    message: 'Hello express jsonp'
+    message: 'Hello express jsonp',
   }
 
-    response.send(`handle(${JSON.stringify(obj)})`)
+  response.send(`handle(${JSON.stringify(obj)})`)
 
   // response.send('Hello express post')
 })
 
-app.listen('8080', ()=>{
+app.listen('8080', () => {
   console.log('服务已启动, 8080端口监听中...')
 })
