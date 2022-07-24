c6d2d77fa8a7f979f1c2ab0df2500022172edb44
849809724@qq.com <84989724@qq.com>
2022-07-24 22:26:24 +0800

    add

--- interviewQuestion/招行od-金赋/2.数组扁平化.js
+++ interviewQuestion/招行od-金赋/2.数组扁平化.js
@@ -1,8 +1,8 @@
 function flatten(arr) {
   return arr.flat(Infinity)
 }
-const result = []
 function pushFn(arr) {
+  const result = []
   for (let i = 0; i < arr.length; i++) {
     if (!Array.isArray(arr[i])) {
       result.push(arr[i])
